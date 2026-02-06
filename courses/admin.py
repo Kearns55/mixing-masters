734f37e86128dd608e9d2f92311eb1c9f92a60fa
Kearns55 <rebekah120@hotmail.com>
2026-02-06 20:29:51 +0100

    Add admin functionality

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Level, SupplyItem, Course
 
 # Register your models here.
+admin.site.register(Level)
+admin.site.register(SupplyItem)
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("name", "level", "price", "max_participants")
+    list_filter = ("level",)
+    search_fields = ("name", "description")
