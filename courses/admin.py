578758b25a8f36adff4ef889a4d890c86fd48578
Kearns55 <rebekah120@hotmail.com>
2026-02-16 11:01:45 +0100

    Add django summernote

--- courses/admin.py
+++ courses/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from .models import Level, SupplyItem, Course
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
 admin.site.register(Level)
 admin.site.register(SupplyItem)
 
+
 @admin.register(Course)
-class CourseAdmin(admin.ModelAdmin):
+class CourseAdmin(SummernoteModelAdmin):
     list_display = ('name', 'level', 'start_date', 'location', 'price', 'max_participants', 'is_active')
     list_filter = ('level', 'is_active')
     search_fields = ('name', 'description', 'location')
+    summernote_fields = ('description',)
