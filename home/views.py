15de045f9e996ef4b18fd4def0c0924b1d92ca57
Kearns55 <rebekah120@hotmail.com>
2026-02-18 13:00:46 +0100

    Fix homepage view

--- home/views.py
+++ home/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from courses.models import Course
 # Create your views here.
 
 
 def home(request):
+    courses = Course.objects.filter(is_active=True)
     return render(request, 'home/index.html')
