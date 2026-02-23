ac449de80277d4d7387849c56bc42ec7e6f607ba
Kearns55 <rebekah120@hotmail.com>
2026-02-23 16:02:41 +0100

    Fix settings to send emails

--- courses/admin.py
+++ courses/admin.py
@@ -7,9 +7,11 @@ admin.site.register(Level)
 admin.site.register(SupplyItem)
 admin.site.register(Purchase)
 
+
 @admin.register(Course)
 class CourseAdmin(SummernoteModelAdmin):
     list_display = ('name', 'level', 'start_date', 'location', 'price', 'max_participants', 'is_active')
     list_filter = ('level', 'is_active')
     search_fields = ('name', 'description', 'location')
     summernote_fields = ('description',)
+
--- mixing_masters/settings.py
+++ mixing_masters/settings.py
@@ -56,16 +56,18 @@ else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_PORT = 587
-    EMAIL_USE_TLS = 'True'
-    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 # Auth + sites
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
-ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
 
 # Middleware
 MIDDLEWARE = [
