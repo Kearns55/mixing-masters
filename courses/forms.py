43271538e8b96d394e6578cbeb1ced4ef59c59e7
Kearns55 <rebekah120@hotmail.com>
2026-03-06 23:51:46 +0100

    Add start date logic to prevent same day events

--- courses/forms.py
+++ courses/forms.py
@@ -1,11 +1,31 @@
 from django import forms
+from django.utils import timezone
+from datetime import timedelta
 from .models import Course, Level, SupplyItem
 
 
 class CourseForm(forms.ModelForm):
+
     class Meta:
         model = Course
         fields = "__all__"
+        widgets = {
+            "start_date": forms.DateTimeInput(
+                attrs={
+                    "type": "datetime-local",
+                }
+            )
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        tomorrow = (timezone.now() + timedelta(days=1)).strftime("%Y-%m-%dT%H:%M")
+
+        # Prevent selecting dates before tomorrow
+        self.fields["start_date"].widget.attrs["min"] = tomorrow
+
+
 class LevelForm(forms.ModelForm):
 
     class Meta:
